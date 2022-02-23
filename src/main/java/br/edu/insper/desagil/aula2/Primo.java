823bbd181593dc399db28056c40176f973983a3a
Enricco Gemha <81189101+G3mha@users.noreply.github.com>
2022-02-23 19:24:31 -0300

    Update Primo.java

--- src/main/java/br/edu/insper/desagil/aula2/Primo.java
+++ src/main/java/br/edu/insper/desagil/aula2/Primo.java
@@ -3,7 +3,18 @@ package br.edu.insper.desagil.aula2;
 public class Primo {
 
 	public int conta(int limite) {
-		return 0;
+		int primo = 0;
+		for (int i = 2; i <= limite; i++) {
+		  boolean tem_primo = true;
+		  for (int x = 2; x < i; x++) {
+			  if (i % x == 0) {
+				  tem_primo = false;
+			  }
+		  }
+		  if (tem_primo == true) {
+			  primo++;
+		  }
+		}
+		return primo;
 	}
-
 }
