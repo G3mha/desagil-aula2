2006505ee098631f08e7fd4386add6014862aa18
Enricco Gemha <81189101+G3mha@users.noreply.github.com>
2022-02-23 19:22:41 -0300

    Update Arvore.java

--- src/main/java/br/edu/insper/desagil/aula2/Arvore.java
+++ src/main/java/br/edu/insper/desagil/aula2/Arvore.java
@@ -3,7 +3,21 @@ package br.edu.insper.desagil.aula2;
 public class Arvore {
 
 	public String constroi(int n) {
-		return "";
+		String arvore = "";
+		for (int height = 0; height < n; height++) {
+	        for (int spaces = n - height - 2; spaces >= 0; spaces--) {
+	            arvore += " ";
+	        }
+	        for (int row = 0; row < height; row++) {
+	            arvore += "/";
+	        }
+	        arvore += "|";
+	        for (int row2 = 0; row2 < height; row2++)
+	        {
+	            arvore += "\\"; 
+	        }
+	        arvore += "\n";
+	    }
+		return arvore;
 	}
-
 }
