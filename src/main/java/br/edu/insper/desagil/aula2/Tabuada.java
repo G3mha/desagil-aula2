25caea34a7986a7f07948a5c89f3c5469480f8e8
Enricco Gemha <81189101+G3mha@users.noreply.github.com>
2022-02-23 19:24:48 -0300

    Update Tabuada.java

--- src/main/java/br/edu/insper/desagil/aula2/Tabuada.java
+++ src/main/java/br/edu/insper/desagil/aula2/Tabuada.java
@@ -3,7 +3,13 @@ package br.edu.insper.desagil.aula2;
 public class Tabuada {
 
 	public String constroi(int n) {
-		return "";
+		String tabuada = "";
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= n; k++) {
+				tabuada = tabuada + (i * k) + " ";
+			}
+			tabuada = tabuada + "\n";
+		}
+		return tabuada;
 	}
-
 }
