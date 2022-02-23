b75a52f3e8ae8cad739bd9b3d2f3438b0bec8183
Enricco Gemha <81189101+G3mha@users.noreply.github.com>
2022-02-23 19:23:31 -0300

    Update Calculadora.java

--- src/main/java/br/edu/insper/desagil/aula2/Calculadora.java
+++ src/main/java/br/edu/insper/desagil/aula2/Calculadora.java
@@ -3,7 +3,28 @@ package br.edu.insper.desagil.aula2;
 public class Calculadora {
 
 	public double executa(int a, int b, char op) {
+		double soma;
+		double aa = a;
+		double bb = b;
+		if (op == '*') {
+			soma = aa * bb;
+			return soma;
+		}
+		else if (op == '+') {
+			soma = aa + bb;
+			return soma; 
+		}
+		else if (op == '-') {
+			soma = aa - bb;
+			return soma;
+		}
+		else if (op == '/') {
+			if (b == 0) {
+				return 0.0;
+			}
+			soma = aa / bb;
+			return soma; 
+		}
 		return 0.0;
 	}
-
 }
