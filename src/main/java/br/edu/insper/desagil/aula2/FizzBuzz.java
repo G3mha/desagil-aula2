1387a6aabf46590aabb983c48fbb7b0375ebdd90
Enricco Gemha <81189101+G3mha@users.noreply.github.com>
2022-02-23 19:24:12 -0300

    Update FizzBuzz.java

--- src/main/java/br/edu/insper/desagil/aula2/FizzBuzz.java
+++ src/main/java/br/edu/insper/desagil/aula2/FizzBuzz.java
@@ -3,7 +3,13 @@ package br.edu.insper.desagil.aula2;
 public class FizzBuzz {
 
 	public String calcula(int n) {
-		return Integer.toString(n);
+		String a = Integer.toString(n);
+		if (n % 3 == 0) {
+			a = "Fizz";
+		}
+		if (n % 5 == 0) {
+			a += "Buzz";
+		}
+		return a;
 	}
-
 }
